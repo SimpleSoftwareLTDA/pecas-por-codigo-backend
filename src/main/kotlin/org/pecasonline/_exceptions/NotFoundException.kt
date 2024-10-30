2ee2884be1e72c2e19dba890cd2122e5fd774b7a
Edcleidson Junior <eddieprofessionalmail@gmai.com>
2024-10-30 02:56:52 -0300

    refactor: refactoring not found exception

--- src/main/kotlin/org/pecasonline/_exceptions/NotFoundException.kt
+++
@@ -1,6 +0,0 @@
-package org.pecasonline._exceptions
-
-class NotFoundException : RuntimeException {
-    constructor(message: String) : super(message)
-    constructor(message: String, cause: Throwable) : super(message, cause)
-}
---
+++ src/main/kotlin/org/pecasonline/common/exceptions/NotFoundException.kt
@@ -0,0 +1,4 @@
+package org.pecasonline.common.exceptions
+
+class NotFoundException(message: String) : RuntimeException(message) {
+}
