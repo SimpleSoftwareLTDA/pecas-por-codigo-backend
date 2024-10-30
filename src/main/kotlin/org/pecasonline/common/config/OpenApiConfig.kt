91289677e773675771584368667d98067f27599e
Edcleidson Junior <eddieprofessionalmail@gmai.com>
2024-10-29 23:29:31 -0300

    chore: creating swagger config

---
+++ src/main/kotlin/org/pecasonline/common/config/OpenApiConfig.kt
@@ -0,0 +1,20 @@
+package org.pecasonline.common.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info().title("Novo Pecas Online API")
+                    .description("API para gerenciamento de peças automotivas.")
+                    .version("1.0")
+            )
+    }
+}
