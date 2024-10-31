252f148e3f224eb7eee74311949ec7e419146b9b
Edcleidson Junior <eddieprofessionalmail@gmai.com>
2024-10-31 02:47:09 -0300

    feature: set cors for any origin

---
+++ src/main/kotlin/org/pecasonline/common/config/CorsConfig.kt
@@ -0,0 +1,23 @@
+package org.pecasonline.common.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration().apply {
+            addAllowedOriginPattern("*")
+            addAllowedHeader("*")
+            addAllowedMethod("*")
+        }
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}
--- src/main/kotlin/org/pecasonline/common/config/WebConfig.kt
+++ src/main/kotlin/org/pecasonline/common/config/WebConfig.kt
@@ -1,17 +1,17 @@
 package org.pecasonline.common.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
+
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOrigins("https://novo-pecas-online-backend-production.up.railway.app")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true)
+        registry.addMapping("/**") // Allows CORS on all paths
+            .allowedOrigins("*") // Accepts requests from any origin
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allows all common HTTP methods
+            .allowedHeaders("*") // Accepts all headers
     }
 }
+
