2bcd2b2b08a638c8f04c26c686a24b219f67aceb
Edcleidson Junior <eddieprofessionalmail@gmai.com>
2024-10-31 02:40:26 -0300

    feature: set default swagger url

--- src/main/kotlin/org/pecasonline/common/config/OpenApiConfig.kt
+++ src/main/kotlin/org/pecasonline/common/config/OpenApiConfig.kt
@@ -3,28 +3,20 @@ package org.pecasonline.common.config
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.servers.Server
-import jakarta.servlet.http.HttpServletRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @Configuration
 class OpenApiConfig {
 
     @Bean
-    fun customOpenAPI(request: HttpServletRequest): OpenAPI {
-        val serverUrl = ServletUriComponentsBuilder.fromRequestUri(request)
-            .scheme("https")
-            .replacePath(null)
-            .build()
-            .toUriString()
-
+    fun customOpenAPI(): OpenAPI {
         return OpenAPI()
-            .addServersItem(Server().url(serverUrl))
             .info(
                 Info().title("Novo Pecas Online API")
                     .description("API para gerenciamento de peças automotivas.")
                     .version("1.0")
             )
+            .addServersItem(Server().url("https://novo-pecas-online-backend-production.up.railway.app"))
     }
 }
