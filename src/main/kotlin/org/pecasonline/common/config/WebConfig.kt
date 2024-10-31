3c40d722dfa4198314c7336f9f97beb2c8ea0abf
Edcleidson Junior <eddieprofessionalmail@gmai.com>
2024-10-31 02:22:58 -0300

    feature: control cors

---
+++ src/main/kotlin/org/pecasonline/common/config/WebConfig.kt
@@ -0,0 +1,18 @@
+package org.pecasonline.common.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "https://novo-pecas-online-backend-production.up.railway.app",
+                "http://novo-pecas-online-backend-production.up.railway.app"
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+    }
+}
