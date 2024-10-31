af961341b8d77311b85b5c8ec511d572f30355a2
Edcleidson Junior <eddieprofessionalmail@gmai.com>
2024-10-31 02:34:48 -0300

    feature: control cors

--- src/main/kotlin/org/pecasonline/common/config/OpenApiConfig.kt
+++ src/main/kotlin/org/pecasonline/common/config/OpenApiConfig.kt
@@ -2,15 +2,25 @@ package org.pecasonline.common.config
 
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @Configuration
 class OpenApiConfig {
 
     @Bean
-    fun customOpenAPI(): OpenAPI {
+    fun customOpenAPI(request: HttpServletRequest): OpenAPI {
+        val serverUrl = ServletUriComponentsBuilder.fromRequestUri(request)
+            .scheme("https")
+            .replacePath(null)
+            .build()
+            .toUriString()
+
         return OpenAPI()
+            .addServersItem(Server().url(serverUrl))
             .info(
                 Info().title("Novo Pecas Online API")
                     .description("API para gerenciamento de peças automotivas.")
