00cfac7a5086ffde629ec92a4174a8c77aa2c377
Robson Cassiano <robson.cassiano@pm.me>
2025-01-25 15:36:26 -0300

    feat: recebe formulário de contato

--- src/main/kotlin/org/pecasonline/features/items/IIitemService.kt
+++ src/main/kotlin/org/pecasonline/features/items/IIitemService.kt
@@ -8,14 +8,3 @@ interface IIitemService {
     fun findItemByDescription(descricao: String, page: Int?=0, size: Int?=10): Page<Item>
     fun findItemByCode(code: String, page: Int?=0, size: Int?=10): Page<Item>
 }
-
-fun generateListWithDuplicates(): List<Int> {
-    val originalItems = (1..80).toList() // 80 itens únicos
-    val duplicates = (1..20).toList() // 20 itens para duplicar
-    return (originalItems + duplicates).shuffled() // Mistura os itens para simular duplicados
-}
-
-fun main() {
-    val listWithDuplicates = generateListWithDuplicates()
-    println(listWithDuplicates)
-}
