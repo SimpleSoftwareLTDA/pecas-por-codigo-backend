4b579af2577fe61bd7460846767c4407abb319b7
Robson Cassiano <robson.cassiano@pm.me>
2025-03-27 09:45:52 -0300

    feat: set banner url

--- src/main/kotlin/org/pecasonline/features/subscription/repository/SubscriptionRepository.kt
+++ src/main/kotlin/org/pecasonline/features/subscription/repository/SubscriptionRepository.kt
@@ -2,12 +2,25 @@ package org.pecasonline.features.subscription.repository
 
 import org.pecasonline.features.subscription.entities.Subscription
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
 interface SubscriptionRepository: JpaRepository<Subscription, Long> {
 
     @Query("SELECT s.bigBannerUrl FROM signature s")
     fun findBigBannerUrls(): List<String?>
+
+    @Transactional
+    @Modifying
+    @Query("""
+        UPDATE signature 
+        SET big_banner_url = :bigBannerUrl
+        WHERE supplier_id = (SELECT s.id FROM supplier s WHERE s.cnpj = :cnpj)
+    """, nativeQuery = true)
+    fun updateBigBannerUrlByCnpj(@Param("cnpj") cnpj: String, @Param("bigBannerUrl") bigBannerUrl: String): Int
+
 }
