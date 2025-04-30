3a37c53e91bd0e1f69adcd10f644f6ca6b9e0b52
Robson Cassiano <robson.cassiano@pm.me>
2025-04-30 03:11:19 -0300

    feat: caching

--- src/main/kotlin/org/pecasonline/NovoPecasOnlineApplication.kt
+++ src/main/kotlin/org/pecasonline/NovoPecasOnlineApplication.kt
@@ -2,6 +2,7 @@ package org.pecasonline
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@EnableCaching
 @EnableAsync
 class NovoPecasOnlineApplication
 
