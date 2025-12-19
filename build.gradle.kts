990daa7aa72df87593d4dfdf5b96bdccae2fe7a4
Robson Cassiano <robson.cassiano@pm.me>
2025-12-19 10:38:06 -0300

    chore: upgrade spring boot from 3.5.9 to 4.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.6"
     id("io.sentry.jvm.gradle") version "5.12.2"
 
-    id("org.springframework.boot") version "3.5.9"
+    id("org.springframework.boot") version "4.0.1"
 
 }
 
