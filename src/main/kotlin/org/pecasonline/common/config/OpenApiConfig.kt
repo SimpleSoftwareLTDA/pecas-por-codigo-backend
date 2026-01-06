54e2f62edc0b5c1b2293d4171fd33c29432c8f24
Robson Cassiano <robson.cassiano@pm.me>
2026-01-06 20:01:54 -0300

    feat: Add initial Spring Boot application, OpenAPI, and local development configurations.

--- src/main/kotlin/org/pecasonline/common/config/OpenApiConfig.kt
+++ src/main/kotlin/org/pecasonline/common/config/OpenApiConfig.kt
@@ -7,25 +7,32 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@Configuration
-class OpenApiConfig {
+import org.springframework.core.env.Environment
 
-    @Value("\${app.url}")
+@Configuration
+class OpenApiConfig(private val env: Environment) {
+
+    @Value("${app.url}")
     private lateinit var currentURL: String
 
     @Bean
     fun customOpenAPI(): OpenAPI {
-        val openApi = OpenAPI()
+        val finalURL = if (currentURL.isNotBlank() && !currentURL.contains("localhost")) {
+            currentURL
+        } else if (env.activeProfiles.contains("prod")) {
+            "https://backend.pecasporcodigo.com.br/"
+        } else {
+            "http://localhost:8080"
+        }
+
+        println("OpenApiConfig: Final Swagger URL is: $finalURL")
+        
+        return OpenAPI()
             .info(
                 Info().title("Peças Por Código API")
                     .description("API de Catalogo de peças automotivas e seus fornecedores.")
                     .version("1.0")
             )
-
-        if (currentURL.isNotBlank() && !currentURL.contains("localhost")) {
-            openApi.addServersItem(Server().url(currentURL))
-        }
-
-        return openApi
+            .servers(listOf(Server().url(finalURL)))
     }
 }
